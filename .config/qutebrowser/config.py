36ecb2bd7398be3c6862cd07f91c72d4de454583
Julien de Carufel <julien.decarufel@gmail.com>
2018-10-14 22:09:27 -0400

    rm data

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -5,3 +5,4 @@ c.content.geolocation = True
 c.url.default_page = 'about:blank'
 c.url.start_pages = 'about:blank'
 c.url.searchengines = {'DEFAULT': 'https://google.com/search?q={}'}
+c.content.user_stylesheets = '~/.config/nord.css'
